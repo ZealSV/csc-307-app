3eeed3087ffbdd5374b8263a74f33bc8127a9890
ZealSV <54489933+ZealSV@users.noreply.github.com>
2024-04-12 21:07:06 -0700

    debugged

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -17,18 +17,13 @@ function TableBody(props) {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   });
-  return (
-    <tr key={index}>
-      <td>{row.name}</td>
-      <td>{row.job}</td>
-      <td>
-        <button onClick={() => props.removeCharacter(index)}>Delete</button>
-      </td>
-    </tr>
-  );
+  return <tbody>{rows}</tbody>;
 }
 
 function Table(props) {
