b56bf4ce693cf4c5d14e582bcebad8da0eaec44c
ZealSV <54489933+ZealSV@users.noreply.github.com>
2024-04-19 15:24:19 -0700

    IE3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -50,6 +51,7 @@ const addUser = (user) => {
   users["users_list"].push(user);
   return user;
 };
+app.use(cors());
 
 app.use(express.json());
 
